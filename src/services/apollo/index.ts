c9b5598144bbdade4f823243e702044af2084605
Artem Oleinikov <artem.oleinikov.web@gmail.com>
2023-11-10 20:57:42 +0200

    add 'refreshToken' to the authService

--- src/services/apollo/index.ts
+++ src/services/apollo/index.ts
@@ -1,12 +1,36 @@
 import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
+import { setContext } from '@apollo/client/link/context'
+
+import { getRefreshedAccessToken } from '../authService'
 
 const httpLink = createHttpLink({
   uri: 'http://localhost:3000/graphql'
 })
 
+const authLink = setContext(async (_, { headers, ...rest }) => {
+  try {
+    const token = await getRefreshedAccessToken()
+
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    }
+  } catch (e) {
+    console.log(e)
+  }
+})
+
+const link = authLink.concat(httpLink)
 const cache = new InMemoryCache()
 
 export const apolloClient = new ApolloClient({
+  link,
+  cache
+})
+
+export const apolloClientAddition = new ApolloClient({
   link: httpLink,
   cache
 })
